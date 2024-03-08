e33e5099ce7a5427824988618dea27e8c5046190
William Cao(Phuoc) <102915662+SuroFK@users.noreply.github.com>
2024-03-07 20:57:53 -0600

    remove stuff (#5)

--- car-details/src/App.tsx
+++ car-details/src/App.tsx
@@ -1,16 +1,13 @@
 import HomePage from "./Pages/Home-Page";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Testing from "./Pages/Testing";
-import Layout from "./Pages/Layout";
 
 export default function App() {
   return(
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Layout />}> */}
           <Route index element={<HomePage />} />
           <Route path="/test" element = {<Testing/>} />
-        {/* </Route> */}
       </Routes>
     </BrowserRouter>
   )
--- car-details/src/Pages/Layout.tsx
+++
@@ -1,7 +0,0 @@
-const Layout = () => {
-    return(
-        <h4>Layout</h4>
-    )
-};
-
-export default Layout;
