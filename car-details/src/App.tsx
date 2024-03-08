73c93ddcb18594ddce68a321095043b69957afa0
William Cao(Phuoc) <102915662+SuroFK@users.noreply.github.com>
2024-03-07 20:21:03 -0600

    working route

--- car-details/src/App.tsx
+++ car-details/src/App.tsx
@@ -1,9 +1,19 @@
-import HomePage from "./Pages/HomePage";
+import HomePage from "./Pages/Home-Page";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Testing from "./Pages/Testing";
+import Layout from "./Pages/Layout";
 
 export default function App() {
   return(
-    <HomePage/>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Layout />}> */}
+          <Route index element={<HomePage />} />
+          <Route path="/test" element = {<Testing/>} />
+        {/* </Route> */}
+      </Routes>
+    </BrowserRouter>
   )
-}
+};
 
 
